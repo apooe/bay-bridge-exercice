1e3678be6a4cda218396c55cb3aa1949a6b3eea8
Laurie Freva <lauriefr@std.hac.ac.il>
2022-02-24 01:26:39 +0200

    add loader

---
+++ src/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import loader from './loader.gif'
+
+const Loader = () => {
+    return (
+        <div className={'d-flex justify-content-center align-items-center text-center p-5'} style={{height: '100vh'}}>
+            <img src={loader} alt="loader"/>
+        </div>
+    );
+};
+
+export default Loader;
